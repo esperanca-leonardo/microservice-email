30f4358de6ce4c19262fc208965bbd3b21325b6f
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-05-23 18:39:51 -0300

    Refactored EmailHelperImpl to retrieve email sender address from application properties.

--- src/main/java/com/esperanca/microservices/email/core/utils/helpers/email/EmailHelperImpl.java
+++ src/main/java/com/esperanca/microservices/email/core/utils/helpers/email/EmailHelperImpl.java
@@ -3,6 +3,7 @@ package com.esperanca.microservices.email.core.utils.helpers.email;
 import com.esperanca.microservices.email.domain.email.entity.Email;
 import com.esperanca.microservices.email.domain.email.enums.StatusEmail;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,14 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class EmailHelperImpl implements EmailHelper {
 
+    private final Environment environment;
     private final SimpleMailMessage message;
 
     @Override
     public SimpleMailMessage createMessage(Email email) {
-        this.message.setFrom(email.getEmailFrom());
+        final String emailFrom = this.environment.getProperty("spring.mail.username");
+
+        this.message.setFrom(emailFrom);
         this.message.setTo(email.getEmailTo());
         this.message.setSubject(email.getSubject());
         this.message.setText(email.getText());
